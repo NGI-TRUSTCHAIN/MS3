2a4e441bd4c316d9e25ce698232cac2087c2c642
GunnerAg <gunner.andersen.gil@gmail.com>
2025-03-17 17:52:22 +0100

    wip

--- packages/wallet/src/adapters/web3authWallet.registration.ts
+++ packages/wallet/src/adapters/web3authWallet.registration.ts
@@ -1,6 +1,6 @@
 import { WalletType } from '../types/enums';
 import { adapterRegistry } from './registry';
-import { Web3AuthWalletAdapter } from './web3auth-newWallet';
+import { Web3AuthWalletAdapter } from './web3authWallet';
 
 /**
  * Register the Web3AuthWalletAdapter with the adapter registry
