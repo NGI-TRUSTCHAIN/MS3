e288a6e8161702bad0c7504b649cc8afcf3b3351
GunnerAg <gunner.andersen.gil@gmail.com>
2025-03-17 16:46:29 +0100

    Fix pipeline

--- packages/wallet/src/adapters/web3authWallet.registration.ts
+++ packages/wallet/src/adapters/web3authWallet.registration.ts
@@ -2,6 +2,9 @@ import { WalletType } from '../types/enums';
 import { adapterRegistry } from './registry';
 import { Web3AuthWalletAdapter } from './web3authWallet';
 
+/**
+ * Register the Web3AuthWalletAdapter with the adapter registry
+ */
 adapterRegistry.register({
   name: "web3auth",
   adapterType: WalletType['evm'], // Web3Auth is EVM-compatible
