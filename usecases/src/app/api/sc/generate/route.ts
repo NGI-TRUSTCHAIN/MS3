7ff44fed8f5b8390edbe9a6c6ef31451379635d4
Angela Herrador <angelaherrador1991@gmail.com>
2025-07-14 11:12:07 +0200

    fix: use-case 2

--- usecases/src/app/api/sc/generate/route.ts
+++ usecases/src/app/api/sc/generate/route.ts
@@ -1,61 +1,23 @@
-import { createContractHandler } from "@m3s/smart-contract";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const { contractType } = await req.json();
   
   try {
-    // Crear el handler del contrato con OpenZeppelin
-    const contractHandler = await createContractHandler({
-      name: "openZeppelin",
-      version: "1.0.0",
-      options: {
-        preserveOutput: true
-      },
-    });
-
-    // Configurar la entrada según el tipo de contrato
+    // Create contract with createContractHandler
+    // Configure input according to the type of contract
     let generationInput;
 
     if (contractType === "erc20") {
-      generationInput = {
-        language: "solidity",
-        template: "openzeppelin_erc20",
-        options: {
-          name: "M3S",
-          symbol: "M3S",
-          premint: "1000",
-          burnable: true
-        },
-      };
     } else {
-      generationInput = {
-        language: "solidity",
-        template: "openzeppelin_erc721",
-        options: {
-          name: "M3S",
-          symbol: "M3S",
-        },
-      };
     }
 
-    // Generar el código fuente
-    const sourceCode = await contractHandler.generateContract(generationInput);
+    // Generate source code with generateContract fc
+    // Create compilationInput with sourceCode, language ("solidity") and contractName. Use that params in compile fc
+    // Deploy contract with getRegularDeploymentData fc 
 
-    const compilationInput = {
-      sourceCode,
-      language: "solidity",
-      contractName: "M3S",
-    };
-
-    // Compila el contrato
-    const compiledOutput = await contractHandler.compile(compilationInput);
-    console.log("compiledOutput", compiledOutput);
-
-    const deployedOutput = await compiledOutput.getRegularDeploymentData();
-    console.log('deployedOutput', deployedOutput);
-
-    return NextResponse.json({ contractHandler, sourceCode, compiledOutput, deployedOutput });
+    //Return contractHandler, sourceCode, compiledOutput, deployedOutput
+    return NextResponse.json({});
   } catch (error) {
     console.error("Error generating contract:", error);
     return NextResponse.json(
